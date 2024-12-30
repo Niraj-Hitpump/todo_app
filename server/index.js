f813a55c1ae55ad1a87d10e9e5e0bf5f8e7568c9
Niraj <nirajkumarprajapati32@gmail.com>
2024-12-30 14:25:21 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://todo-app.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
